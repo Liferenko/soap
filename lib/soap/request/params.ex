968c4a146c9abb6d5af27a25f532bb4ea18789e3
Paul Liferenko <eleven.krsk@gmail.com>
2021-02-25 17:15:54 +0200

    add right riamethods

--- lib/soap/request/params.ex
+++ lib/soap/request/params.ex
@@ -288,7 +288,7 @@ defmodule Soap.Request.Params do
   defp add_our_hardcoded_attributes() do
     %{
         "xmlns:env" => "http://www.w3.org/2003/05/soap-envelope", 
-        "xmlns:ns1" => "urn:riamethods-bonus",
+        "xmlns:ns1" => "urn:riamethods-make-order#r",
         "xmlns:xsd" => "http://www.w3.org/2001/XMLSchema",
         "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance",
         "xmlns:enc" => "http://www.w3.org/2003/05/soap-encoding",
